e49309bd69f8b3ae46697534a01078b95f7cc63a
Oskin.K <constantoskin@gmail.com>
2021-03-14 10:36:04 +0300

    Add simple list with custom elements

--- javascript-3/src/index.js
+++ javascript-3/src/index.js
@@ -1,3 +1,45 @@
-import "./style.scss";
+import { simpleTree } from './mock';
+import './components/my-tree';
+import './components/my-leaf';
 
 console.log('Hola Amigo');
+
+const cunstructTree = (arr, parentId) => {
+    const { id, items } = arr[0];
+
+    createTree(id, parentId);
+
+    if (!items) {
+        return;
+    }
+
+    cunstructTree(items, id);
+}
+
+const createTree = (id, parentId) => {
+    const list = document.createElement('my-tree');
+    const listItemTree = createItem();
+    const listItemId = createItem(`id: ${id}`);
+
+    list.id = id;
+    list.appendChild(listItemId);
+    list.appendChild(listItemTree);
+
+    if (parentId) {
+        document.getElementById(parentId).querySelector('my-leaf:last-child').appendChild(list);
+    } else {
+        document.body.appendChild(list);
+    }
+}
+
+const createItem = (value) => {
+    const item = document.createElement('my-leaf');
+
+    if (value) {
+        item.textContent = value;
+    }
+
+    return item;
+}
+
+cunstructTree(simpleTree);
